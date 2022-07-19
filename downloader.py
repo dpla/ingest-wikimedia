66ed0b9d678e9fc3c89cce8a2efcf9a430654815
scott <scott.brandon.williams@gmail.com>
2022-07-19 10:37:26 -0600

    Fix logging message

--- downloader.py
+++ downloader.py
@@ -99,8 +99,7 @@ ids = list()
 if file_filter:
     with open(file_filter) as f:
         ids = [line.rstrip() for line in f]
-
-logger.info(f"{len(ids)} IDs to download")
+    logger.info(f"{len(ids)} IDs to download")
 
 # Get individual parquet files from ingestion3 wiki output
 file_list = utils.get_parquet_files(path=input_df)
