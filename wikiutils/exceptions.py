f1643a4807bf1d96f0679360c69edade91b929e1
scott <scott.brandon.williams@gmail.com>
2023-07-13 03:55:18 -0600

    Add exceptions

---
+++ wikiutils/exceptions.py
@@ -0,0 +1,17 @@
+"""
+This module contains all the exceptions used in the wikiutils package.
+"""
+
+class UploadException(Exception):
+    """
+    Base class for exceptions in this module.
+    """
+    def __init__(self, message):
+        super().__init__(message)
+
+class DownloadException(Exception):
+    """
+    Base class for exceptions in this module.
+    """
+    def __init__(self, message):
+        super().__init__(message)
