dbaf2e3a479a4d7434760941dfed2f3e986708f0
scott <scott.brandon.williams@gmail.com>
2023-08-09 15:31:55 -0600

    Huge rewrite of the downloader Improved logging Cleanup a lot of code

--- wikiutils/utils.py
+++ wikiutils/utils.py
@@ -20,8 +20,7 @@ import requests
 
 from awswrangler import s3
 from botocore.config import Config
-from botocore.exceptions import ClientError
-
+from botocore.exceptions import ClientError, ConnectionError
 
 class Utils:
     """General utility functions for Wikimedia project"""
@@ -45,87 +44,144 @@ class Utils:
         :param path:
         :return:
         """
+        # WTF is this doing? I'm really not sure what or why this exists. 
+        # This doesn't seem like it is necessary for s3 since there is not need to 
+        # create a path for s3.
         prefix = b's3' if isinstance(path, bytes) else "s3"
         if not path.startswith(prefix) and not Path(path).exists():
             Path(path).mkdir(parents=True)
 
-    def download(self, url, out):
+    def download(self, source, destination):
         """
-
-        :param url: URL of asset to download
-        :param out: Full path to save asset
-        :return: url, time to process download, filesize (in bytes), name
+        Download asset to local file system or s3
+        
+        :param source: URL of asset to download
+        :param destination: Full path to save asset
+        :return:    url
+                    time to process download
+                    filesize (in bytes)
+                    name
         """
-        url = url if isinstance(url, bytes) else url.encode('utf-8')
+        # Endcode source to bytes if it is not already
+        # source = source if isinstance(source, bytes) else source.encode('utf-8')
         try:
-            if out.startswith("s3"):
-                return self.download_s3(url=url, out=out)
-            return self.download_local(url=url, file=out, overwrite=False)
+            # If the destination is s3, then check to see if the file already exists
+            if destination.startswith("s3://"):
+                bucket, key = self.get_bucket_key(destination)
+                exists, size =  self.file_exists_s3(bucket=bucket, key=key)
+                if not exists:
+                    return self.download_to_s3(source=source, bucket=bucket, key=key)
+                return destination, size
+            
+            # If the destination is local file system, then download the file and don't overwrite it
+            else: 
+                return self.download_local(source=source, file=destination)
         except Exception as exception:
-            raise Exception(f"Failed to download %s: %s", url, exception.__str__) from exception
+            raise Exception("Failed to download %s: %s", source, str(exception))
 
-    def download_local(self, url, file, overwrite=False):
+    def get_file_info(self, file, overwrite):
         """
-        Download images locally
+        Does the file exist? If so, return the file path and the file size in bytes
 
-        :param overwrite: Boolean
-        :param url:
+        :param file: Full path to save asset (ex /tmp/image.jpg)
+        :param overwrite: Boolean to overwrite existing downloaded file
+        :return: None, 0 if the image already exists and overwrite is True otherwise 
+                            return the file path. 
+                file, 0 if the file does not exist and needs to be downloaded for 
+                        the first time
+                file, file_size if the file does exist and overwrite is False
+        """
+        if Path(file).exists():
+            file_size = os.path.getsize(file)
+            if overwrite:
+                os.remove(file)
+                return None, 0
+            return file, file_size
+        else:
+            pass
+            # new file to download
+            return file, 0
+   
+    def download_local(self, source, file, overwrite=False):
+        """
+        Download images to local file system and return path to file 
+        and the size of the file in bytes
+
+        :param source: The url to download
+        :param file: Full path to save asset (ex /tmp/image.jpg)
+        :param overwrite: Boolean to overwrite existing downloaded file
         :param filexception:
         :return:
         """
-        exists = Path(file).exists()
+        # self.download_to_temp(file, file_exists, source, overwrite)
+        if overwrite: 
+            os.remove(file)
         try:
-            # Image already exists, do nothing
-            if exists and not overwrite:
-                return file, 0, os.path.getsize(file)
-            if exists:
-                os.remove(file)
-            response = requests.get(url, timeout=30)
+            
+            response = requests.get(source, timeout=30)
             with open(file, 'wb') as f:
                 f.write(response.content)
             file_size = os.path.getsize(file)
-            self.logger.info(f"Download {url}, {self.sizeof_fmt(file_size)}")
             return file, file_size
         except Exception as exception:
-            raise Exception(f"Failed to download local {url}: {exception.__str__}") from exception
+            raise Exception("Error in download_local() %s", str(exception))
 
-    def download_s3(self, url, out):
+    def file_exists_s3(self, bucket, key):
         """
+        Check to see if the file exists in s3
+
+        :param bucket: S3 bucket
+        :param key: S3 key
+        """
+        try:
+            # this does not check to see if the image is the same image, just that the file exists
+            # is should check the md5 hash of the file to see if it is the same and needs to be
+            # uploaded and replace the existing file.
+            response = self.s3.head_object(Bucket=bucket, Key=key)
+            size = response['ContentLength']
+            return True, size
+        except ClientError:
+            # The head request fails therefore we assume the file does not exist in s3
+            return False, 0
+    
+    def get_bucket_key(self, s3_url):
+        """
+        Parse S3 url and return bucket and key
+
+        :param s3_url: S3 url
+        :return: bucket, key
+        """
+        s3_url_parsed = urlparse(s3_url)
+        bucket = s3_url_parsed.netloc
+        key = f"{s3_url_parsed.path.replace('//', '/').lstrip('/')}"
+        return bucket, key
+
+    def download_to_s3(self, source, bucket, key):
+        """
+
+        Tries to download a file from the source url and save it to s3. If the file
+        already exists in s3 then this step is skipped. To achive this 
+        the file is downloaded to a temp file on the local file system and then uploaded
+        to s3. 
 
         :param url:
         :param out:
         :param name
         :return:
         """
-        o = urlparse(out)
-        bucket = o.netloc
-        # Generate full s3 key using file name from url and path generate previously
-        key = f"{o.path.replace('//', '/').lstrip('/')}"
+        # Create temp local file and download source file to it
+        temp_file = tempfile.NamedTemporaryFile(delete=False)
 
+        out, size = self.download_local(source=source, file=temp_file.name, overwrite=True)
+        # Get content type from file, used in metadata for s3 upload
+        content_type = magic.from_file(temp_file.name, mime=True)
         try:
-            response = self.s3.head_object(Bucket=bucket, Key=key)
-            size = response['ContentLength']
-            self.logger.info(f"%s already exists, skipping download", key)
-            return out, size  # Return if file already exists in s3
-        except ClientError as client_error:
-            # swallow exception generated from checking ContentLength on non-existant item
-            # File does not exist in S3, need to download
-            pass
-
-        try:
-            # Create tmp local file for download
-            temp_file = tempfile.NamedTemporaryFile(delete=False)
-            out, size = self.download_local(url=url, file=temp_file.name, overwrite=True)
-            # Get content type from file, used in metadata for s3 upload
-            content_type = magic.from_file(temp_file.name, mime=True)
-            # Upload to s3
-            with open(temp_file.name, "rb") as f:
-                self.s3.upload_fileobj(Fileobj=f, Bucket=bucket, Key=key, ExtraArgs={'ContentType': content_type})
-                self.logger.info(f"Uploaded to s3://{bucket}/{key}")
+            # Upload temp file to s3
+            with open(temp_file.name, "rb") as file:
+                self.s3.upload_fileobj(Fileobj=file, Bucket=bucket, Key=key, ExtraArgs={'ContentType': content_type})
                 return f"s3://{bucket}/{key}", size
         finally:
-            # cleanup temp file
-            os.unlink(temp_file.name)
+            os.unlink(temp_file.name) 
 
     def upload_to_s3(self, bucket, key, file, content_type): 
         """
@@ -157,7 +213,7 @@ class Utils:
         :param columns: Columns to rename
         :return: Dataframe
         """
-        return pd.read_parquet(path, engine='fastparquet').rename(columns=columns)
+        
 
     def get_df(self, path, columns):
         """
@@ -167,7 +223,8 @@ class Utils:
         :param columns: Columns to rename
         :return: Dataframe
         """
-        return self.get_df_local(path, columns)
+        return pd.read_parquet(path, engine='fastparquet').rename(columns=columns)
+        # return self.get_df_local(path, columns)
     
     def get_parquet_files(self, path):
         """
@@ -196,9 +253,8 @@ class Utils:
         :param columns: Columns to write
         :return: None
         """
-        self.logger.info(f"Saving {path}")
-        df_out = pd.DataFrame(data, columns=columns)
-        df_out.to_parquet(path)    
+        self.logger.info("Saving %s", path)
+        pd.DataFrame(data, columns=columns).to_parquet(path)
 
     def get_extension_from_file(self, file):
         """
@@ -224,6 +280,18 @@ class Utils:
         return ext
             
 
+    def get_iiif_manifest(self, url):
+        """
+        :return: JSON object
+        """
+        try:
+            request = requests.get(url, timeout=30)
+            data = request.content
+            return json.loads(data)
+        except ConnectionError as connection_error:
+            # self.logger.error("Unable to request %s: %s", url, str(connection_error))
+            raise Exception(f"Unable to request {url}: {str(connection_error)}")
+
     def get_iiif_urls(self, iiif):
         """
 
@@ -231,47 +299,26 @@ class Utils:
         :return:
         """
         # sequences \ [array] — [0, default]  \ canvases \ [array] \ images \ [array, 0 default] \ resource \ @id
-        canvases = None
-        sequences = None
-        try:
-            request = requests.get(iiif, timeout=10)
-            data = request.content
-            iiif_manifest = json.loads(data)
-            sequences = iiif_manifest['sequences']
-        except ConnectionError as connection_error:
-            self.logger.error("Unable to request %s: %s", iiif, connection_error.__str__)
-            return []
-        except Exception as exception:
-            self.logger.error(f"Unknown error requesting {iiif}: {exception}")
-            return []
-
-        if len(sequences) > 1:
-            # More than one sequence, return empty list and log some kind of message
-            self.logger.info(f"Got more than one IIIF sequence. Unsure of meaning. %s", iiif)
-            return []
-        if len(sequences) == 1:
-            try:
-                canvases = sequences[0]['canvases']
-            except KeyError as key_error:
-                self.logger.info(f'No canvasses defined in %s', iiif)
-            except Exception as exception:
-                self.logger.info("Error extracting canvasses  %s: %s", iiif, exception.__str__)
-
-        if canvases is None:
-            self.logger.info(f"No sequences or canvases in IIIF manifest: {iiif}")
-            return []
-
+        canvases = []
         images_urls = []
+
+        iiif_manifest = self.get_iiif_manifest(iiif)
+        # if 'sequences' in iiif_manifest and there is one sequence value
+        if 'sequences' in iiif_manifest and len(iiif_manifest['sequences']) == 1:
+            canvases = iiif_manifest['sequences'][0]['canvases'] if 'canvases' in iiif_manifest['sequences'][0] else []
+        else: 
+            # More than one sequence, return empty list and log some kind of message
+            self.logger.info("Got more than one IIIF sequence. Unsure of meaning. %s", iiif)
+            return []
+
         for canvas in canvases:
             try:
                 image_url = canvas['images'][0]['resource']['@id']
                 # if missing file extension add it to URL to be requested
                 image_url = image_url if '.' in image_url[image_url.rfind('/'):] else f"{image_url}.jpg"
                 images_urls.append(image_url)
-            except KeyError as key_error:
-                self.logger.info(f'No images defined in  {iiif}')
-            except Exception as exception:
-                self.logger.info(f'Error extracting canvasses  {iiif} because {exception}')
+            except KeyError:
+                self.logger.error("No images defined in %s", iiif)
         return images_urls
 
     def sizeof_fmt(self, num, suffix='B'):
@@ -288,3 +335,8 @@ class Utils:
             num /= 1024.0
         return "%.1f%s%s" % (num, 'Yi', suffix)
     
+    def create_destination_path(self, base, batch, count, dpla_id):
+        """
+        Create destination path to download file to
+        """
+        return f"{base}/batch_{batch}/assets/{dpla_id[0]}/{dpla_id[1]}/{dpla_id[2]}/{dpla_id[3]}/{dpla_id}/{count}_{dpla_id}".strip()
