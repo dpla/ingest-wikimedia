9b7c8a2a1236e3b5e1e793eaa6ee7bf1d329d6ff
scott <scott@dp.la>
2023-08-10 14:27:49 -0600

    Small refactors of the uploading steps (#15)
    Improve documentation and comments

--- wikiutils/uploader.py
+++ wikiutils/uploader.py
@@ -5,14 +5,12 @@ Upload images to Wikimedia Commons
 import mimetypes
 import os
 import tempfile
-from urllib.parse import urlparse
 
 import pywikibot
 import boto3
 import botocore
 
 from wikiutils.exceptions import UploadException
-from wikiutils.logger import WikimediaLogger
 from wikiutils.utils import Utils as WikimediaUtils
 
 class Uploader:
@@ -21,7 +19,7 @@ class Uploader:
     """
     site = None
     log = None
-    s3 = boto3.client('s3')
+    s3 = boto3.resource('s3')
     wikiutils = None
 
     # List of warning codes to ignore. This list exists mainly to exclude 'duplicate' (i.e.,
@@ -40,13 +38,35 @@ class Uploader:
         'was-deleted'
     ]
 
-    def __init__(self, partner_name, logger):
+    def __init__(self, logger):
         self.log = logger
         self.wikiutils = WikimediaUtils()
         self.site = pywikibot.Site()
         self.site.login()
-        self.log.log_info(f"Logged in user is: {self.site.user()}")
+        self.log.log(f"Logged in user is: {self.site.user()}")
 
+    def download(self, bucket, key, destination):
+        """
+        Download file from s3 to local file system
+        
+        :param bucket: s3 bucket
+        :param key: s3 key
+        :param destination: Full path to save the asset
+        :return:    output_path: Full path to downloaded asset
+        """
+        with open(destination.name, "wb") as f:
+            try:
+                self.s3.Bucket(bucket).download_file(key, destination.name)
+                return destination.name
+            except botocore.exceptions.ClientError as client_error:
+                if client_error.response['Error']['Code'] == "404":
+                    raise UploadException(f"Does not exist: {bucket}{key}") from client_error
+                elif client_error.response['Error']['Code'] == "403":
+                    raise UploadException(f"Access denied: {bucket}{key}") from client_error  
+                # TODO include specific client errors here  
+                else:   
+                    raise UploadException(f"Unable to download {bucket}{key} to {destination.name}: {str(client_error)}") from client_error   
+                     
     def upload(self, wiki_file_page, dpla_identifier, text, file, page_title):
         """
 
@@ -57,24 +77,19 @@ class Uploader:
         :return:
         """
         comment = f"Uploading DPLA ID \"[[dpla:{dpla_identifier}|{dpla_identifier}]]\"."
-        temp_file = None
 
         # This is a massive kludge because direct s3 upload via source_url is not allowed.
         # Download from s3 to temp location on box then upload local file to wikimeida
-        if file.startswith("s3"):
-            s3 = boto3.resource('s3')
-            bucket, key = self.wikiutils.get_bucket_key(file)
-        else: 
+        if not file.startswith("s3"):
             raise UploadException("File must be on s3")
+        bucket, key = self.wikiutils.get_bucket_key(file)
+            
 
-        temp_file = tempfile.NamedTemporaryFile(delete=False)
-        with open(temp_file.name, "wb") as f:
-            try:
-                s3.Bucket(bucket).download_file(key, temp_file.name)
-            except botocore.exceptions.ClientError as client_error:
-                if client_error.response['Error']['Code'] == "404":
-                    raise UploadException(f"Does not exist: {bucket}{key}") from client_error
-                raise UploadException(f"Unable to download {bucket}{key} to {temp_file.name}: {str(client_error)}") from client_error
+        # Download to temp file on local file system
+        # Will raise exceptions if the file cannot be downloaded
+        temp_file = tempfile.NamedTemporaryFile()
+        self.download(bucket=bucket, key=key, destination=temp_file)
+
         try:
             self.site.upload(filepage=wiki_file_page,
                              source_filename=temp_file.name,
@@ -84,7 +99,7 @@ class Uploader:
                              asynchronous= True,
                              chunk_size=3000000 # 3MB
                             )
-            self.log.log_info(f"Uploaded '{page_title}'")
+            self.log.info(f"Uploaded '{page_title}'")
             # FIXME this is dumb and should be betterm, it either raises and exception or returns true
             return True 
         except Exception as exception:
@@ -109,13 +124,16 @@ class Uploader:
         - only use the first 181 characters of image file name
         - replace [ with (
         - replace ] with )
+        - replace { with (
+        - replace } with )
         - replace / with -
-
-        :param title:
-        :param dpla_identifier:
-        :param suffix:
-        :param page:
-        :return:
+        - replace : with -
+        
+        :param title: DPLA title
+        :param dpla_identifier: DPLA identifier
+        :param suffix: file suffix
+        :param page: If this is a multi-page document, the page number
+        :return: Propertly escaped and formatted Wikimedia page title
         """
         escaped_title = title[0:181] \
             .replace('[', '(') \
@@ -125,37 +143,41 @@ class Uploader:
             .replace('/', '-') \
             .replace(':', '-') \
 
+        # Check to see if the page contains invisible characters and is invalid
+        # This is probably unnecessary, but it's here just in case
+        if pywikibot.tools.chars.contains_invisible(title):
+            self.log.error(f"Invalid title due to invisible characters: {title}")
+            return None
+        
         # Add pagination to page title if needed
         if page:
             return f"{escaped_title} - DPLA - {dpla_identifier} (page {page}){suffix}"
         return f"{escaped_title} - DPLA - {dpla_identifier}{suffix}"
 
-    # noinspection PyStatementEffect
     def create_wiki_file_page(self, title):
         """
         Create a Wikimedia page for the image if it does not already exist. If it does exist, return None.
 
         :param title: Title of the record in DPLA
-        :return: None if the page already exists, otherwise a pywikibot.FilePage object
+        :return: None if the page already exists or the title , otherwise a pywikibot.FilePage object
         """
-        # Check to see if the page contains invisible characters and is invalid
-        # This is probably unnecessary, but it's here just in case
-        invaild_name = pywikibot.tools.chars.contains_invisible(title)
-        if invaild_name:
-            self.log.log_info(f"Invalid name: {invaild_name}")
-            return None
-
         wiki_page = pywikibot.FilePage(self.site, title=title)
         if wiki_page.exists():
             return None
-        return wiki_page    # Create a function to get the extension from the mime type
+        return wiki_page
     
     def get_metadata(self, row):
         """
-        Get metadata for a DPLA record
+        Get metadata for a DPLA record from the row emitted by the download process
 
-        :param dpla_id: The DPLA identifier
-        :return: A
+        :param dpla_id: Row from the data for a DPLA record and associated image file
+        :return: A tuple containing 
+                    DPLA identifier
+                    Path to the file
+                    Size of the file,
+                    Title of the DPLA record
+                    Wiki markup for the page (generated by ingestion3)
+                    Page if this is a multi-page document
         """
         try:
             dpla_id = getattr(row, 'dpla_id')
@@ -186,5 +208,5 @@ class Uploader:
                 mime = mimetypes.guess_type(path)[0]
             return mimetypes.guess_extension(mime)
         except Exception as exception:
-            raise UploadException(f"Unable to get extension for {path}: {exception.__str__}") from exception
+            raise UploadException(f"Unable to get extension for {path}: {str(exception)}") from exception
         
