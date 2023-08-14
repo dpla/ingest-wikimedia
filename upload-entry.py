21eb6f5033517344eb8fbee9b923407e02309dfa
scott <scott.brandon.williams@gmail.com>
2023-08-13 20:13:41 -0600

    Faster id lookup Linting Set attempted count

--- upload-entry.py
+++ upload-entry.py
@@ -1,13 +1,14 @@
 """
 Upload to Wikimedia Commons
 
-This needs a "batch" folder for input 
-Read parquet file and then upload assets 
+This needs a "batch" folder for input
+Read parquet file and then upload assets
 
 """
 import getopt
 import sys
 import boto3
+import numpy as np
 
 from wikiutils.utils import Utils
 from wikiutils.logger import WikimediaLogger
@@ -19,7 +20,6 @@ utils = Utils()
 status = UploadStatus()
 
 partner_name, input_df = None, None
-failed_count, upload_count, skip_count, total_count = 0, 0, 0, 0
 columns = {
             "dpla_id": "dpla_id",
             "path": "path",
@@ -31,8 +31,8 @@ columns = {
 
 # Get input parameters
 try:
-    opts, args = getopt.getopt(sys.argv[1:], 
-                               "hi:u:o:", 
+    opts, args = getopt.getopt(sys.argv[1:],
+                               "hi:u:o:",
                                ["input=",
                                 "partner="])
 except getopt.GetoptError:
@@ -54,7 +54,11 @@ uploader = Uploader(log)
 
 log.info(f"Input: {input_df}")
 data_in = utils.read_parquet(input_df)
-total_count = len(data_in)
+status.set_total(len(data_in))
+
+# Get unique dpla_ids and counts
+unique, counts = np.unique(data_in["dpla_id"], return_counts=True)
+unique_ids = dict(zip(unique, counts))
 
 for row in data_in.itertuples(index=columns):
     dpla_id, path, size, title, wiki_markup = None, None, None, None, None
@@ -63,14 +67,14 @@ for row in data_in.itertuples(index=columns):
         dpla_id, path, size, title, wiki_markup, page = uploader.get_metadata(row)
         # If there is only one record for this dpla_id, then page is `None` and pagination will not
         # be used in the Wikimedia page title
-        page = None if len(data_in.loc[data_in['dpla_id'] == dpla_id]) == 1 else page
+        page = None if unique_ids[dpla_id] == 1 else page
         # Get file extension
         ext = uploader.get_extension(path)
         # Create Wikimedia page title
         page_title = uploader.create_wiki_page_title(title=title,
-                                                dpla_identifier=dpla_id,
-                                                suffix=ext,
-                                                page=page)
+                                                     dpla_identifier=dpla_id,
+                                                     suffix=ext,
+                                                     page=page)
 
         # Create wiki page using Wikimedia page title
         wiki_page = uploader.create_wiki_file_page(title=page_title)
@@ -82,26 +86,24 @@ for row in data_in.itertuples(index=columns):
             continue
 
         # Upload image to wiki page
-        # FIXME -- Commented out for --dry-run testing 
+        # FIXME -- Commented out for --dry-run testing
         uploader.upload(wiki_file_page=wiki_page,
                         dpla_identifier=dpla_id,
                         text=wiki_markup,
                         file=path,
-                        page_title=page_title
-                        )
-        upload_count += 1
+                        page_title=page_title)
     except UploadException as upload_exec:
         log.error("Upload error: %s", str(upload_exec))
         status.increment(UploadStatus.FAILED)
         continue
     except Exception as exception:
-        log.error("Unknown error: %s", str(exception))
+        log.error(f"Unknown error: {str(exception)}")
         status.increment(UploadStatus.FAILED)
         continue
 
 # Summarize upload
 log.info(f"Finished upload for {input_df}")
-log.info(f"Attempted: {status.skip_count + status.fail_count + status.upload_count} file")
+log.info(f"Attempted: {status.attempted} files")
 log.info(f"Uploaded {status.upload_count} new files")
 log.info(f"Failed {status.fail_count} files")
 log.info(f"Skipped {status.skip_count} files")
@@ -115,11 +117,13 @@ log.info("Fin.")
 # Send email notification
 ses_client = boto3.client('ses', region_name='us-east-1')
 emailer = SesMailSender(ses_client)
-summary = UploadSummary(partner_name=partner_name, log_url=public_url, total_upload=0, status=status)
-
-emailer.send_email(source="tech@dp.la",
-                   destination=SesDestination(tos=["scott@dp.la"]),  # FIXME dominic@dp.la should be here. Who else? 
+summary = UploadSummary(partner_name=partner_name,
+                        log_url=public_url,
+                        total_upload=0,
+                        status=status)
+emailer.send_email(source="DPLA Tech Bot<tech@dp.la>",
+                   destination=SesDestination(tos=["scott@dp.la"]),  # FIXME dominic@dp.la should be here. Who else?
                    subject=summary.subject(),
                    text=summary.body_text(),
                    html=summary.body_html(),
-                   reply_tos=["tech@dp.la"])
+                   reply_tos=["DPLA Tech Bot<tech@dp.la>"])
