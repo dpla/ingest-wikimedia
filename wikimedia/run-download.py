50298c73fbfe139e826465c772820ca70b521c7d
scott <scott@dp.la>
2023-08-21 09:27:26 -0600

    Most recent input (#21)
    * Get most recent for download input (read from ingestion3 /wiki/
    
    * Update README
    
    * Remove unused import

--- wikimedia/run-download.py
+++ wikimedia/run-download.py
@@ -6,7 +6,6 @@ import sys
 import boto3
 
 from utilities.fs import S3Helper, log_file
-from utilities.logger import WikimediaLogger
 from utilities.emailer import SesMailSender, SesDestination, DownloadSummary
 from utilities.arguements import get_download_args
 from entries.download import DownloadEntry
@@ -21,13 +20,15 @@ def main():
     EMAIL_TO = ["scott@dp.la"]
 
     s3 = S3Helper()
+
+    # Get the most recent parquet file from the input path
+    bucket, key = s3.get_bucket_key(args.get('input_data'))
+    recent_key = s3.most_recent(bucket=bucket, key=key)
+    args['input_data'] = f"s3://{bucket}/{recent_key}"
+
     entry = DownloadEntry(args)
 
     file = log_file(partner_name=args.get('partner_name'), event_type="download")
-
-    print(file)
-
-    # log_config = logging.basicConfig()
 
     log = logging
     log.basicConfig(level=logging.INFO,
---
+++ wikimedia/run-upload.py
@@ -0,0 +1,78 @@
+"""
+Upload to Wikimedia Commons
+
+"""
+import getopt
+import sys
+import boto3
+
+from utilities.fs import FileSystem, S3Helper
+# from utilities.logger import WikimediaLogger
+from executors.uploader import Uploader
+from utilities.emailer import SesMailSender, SesDestination, UploadSummary
+
+# Get input parameters
+partner_name, input_path = None, None
+try:
+    opts, args = getopt.getopt(sys.argv[1:],
+                               "hi:u:o:",
+                               ["input=",
+                                "partner="])
+except getopt.GetoptError:
+    print('upload-entry.py --partner <dpla partner abbreviation> --input <path to parquet>')
+    sys.exit(2)
+
+for opt, arg in opts:
+    if opt == '-h':
+        print(
+            'upload-entry.py --partner <DPLA hub abbreviation> --input <path to parquet>')
+        sys.exit()
+    elif opt in ("-i", "--input"):
+        input_path = arg
+    elif opt in ("-p", "--partner"):
+        partner_name = arg
+
+log = WikimediaLogger(partner_name=partner_name, event_type="upload")
+uploader = Uploader(log)
+fs = FileSystem()
+s3 = S3Helper()
+
+# This is the schema emitted by the Wikimedia ingest download process
+READ_COLUMNS = {"_1": "dpla_id",
+                "_2": "path",
+                "_3": "size",
+                "_4": "title",
+                "_5": "markup",
+                "_6": "page"}
+
+data_in = fs.read_parquet(input_path, cols=READ_COLUMNS)
+
+log.info(f"Read {len(data_in)} from {input_path}")
+uploader.execute_upload(data_in, READ_COLUMNS)
+
+# Summarize upload
+status = uploader.get_status()
+log.info(f"Attempted: {status.attempted} files for {status.dpla_count} DPLA records")
+log.info(f"Uploaded {status.upload_count} new files")
+log.info(f"Failed {status.fail_count} files")
+log.info(f"Skipped {status.skip_count} files")
+
+# Upload log file to s3
+bucket, key = s3.get_bucket_key(input_path)
+public_url = log.write_log_s3(bucket=bucket, key=key)
+log.info(f"Log file saved to {public_url}")
+log.info("Fin")
+
+# Send email notification
+ses_client = boto3.client('ses', region_name='us-east-1')
+emailer = SesMailSender(ses_client)
+summary = UploadSummary(partner=partner_name,
+                        log_url=public_url,
+                        status=status)
+
+emailer.send_email(source="DPLA Tech Bot<tech@dp.la>",
+                   destination=SesDestination(tos=["scott@dp.la"]),  # FIXME dominic@dp.la should be here. Who else?
+                   subject=summary.subject(),
+                   text=summary.body_text(),
+                   html=summary.body_html(),
+                   reply_tos=["DPLA Tech Bot<tech@dp.la>"])
